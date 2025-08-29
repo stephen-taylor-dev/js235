f7e9c5053d0f756f28c7eeb3ecb92548db364c45
Stephen Taylor <stephenmtaylor93@gmail.com>
2025-08-29 17:17:30 -0400

    starting'

---
+++ callbacks.js
@@ -0,0 +1,60 @@
+// function basicCallback(callback, num) {
+//   setTimeout(() => callback(num), 2 * 1000);
+// }
+
+// // Example usage:
+// basicCallback((number) => {
+//   console.log(number * 2);
+// }, 5);
+// // After 2 seconds, logs: 10
+
+// function downloadFile(callback) {
+//   console.log('Downloading file...');
+//   setTimeout(() => callback('Download complete!'), 1500);
+// }
+
+// downloadFile((message) => console.log(message));
+
+
+// function processData(nums, callback) {
+//   setTimeout(() => {
+//     const newNums = nums.map(callback)
+//     console.log(newNums)
+//   }, 1000);
+// }
+
+// // Example usage:
+// processData([1, 2, 3], (number) => number * 2);
+// // After 1 second, logs: [2, 4, 6]
+
+// function waterfallOverCallbacks(arr, initialVal) {
+//   arr.forEach(callback => {
+//     initialVal = callback(initialVal);
+//   });
+
+//   console.log(initialVal);
+// }
+
+
+// // Example usage:
+// const double = (x) => x * 2;
+// waterfallOverCallbacks([double, double, double], 1);
+// // Logs: 8
+
+
+function startCounter(callback) {
+  let count = 0;
+  let id = setInterval(() => {
+    count += 1;
+    if (callback(count)) {
+      clearInterval(id);
+    }
+  }, 1000);
+}
+
+// Example usage:
+startCounter((count) => {
+  console.log(count);
+  return count === 5;
+});
+// Logs 1, 2, 3, 4, 5, then stops
---
+++ practice.js
@@ -0,0 +1,83 @@
+// function delayLog() {
+//   for (let i = 1; i <= 10; i += 1) {
+//     setTimeout(() => {
+//        console.log(i);
+//     }, i*1000);
+//   }
+// }
+
+// delayLog();
+
+
+// setTimeout(() => {        // 1
+//   console.log('Once');    // 5
+// }, 1000);
+
+// setTimeout(() => {        // 2
+//   console.log('upon');    // 7  
+// }, 3000);
+
+// setTimeout(() => {        // 3
+//   console.log('a');       // 6
+// }, 2000);
+
+// setTimeout(() => {      // 4
+//   console.log('time');   // 8
+// }, 4000);
+
+
+
+// setTimeout(() => {        // 1
+//   setTimeout(() => {      // 6
+//     q();                  // 12
+//   }, 15);
+
+//   d();                    // 7
+
+//   setTimeout(() => {      // 8
+//     n();                  // 10
+//   }, 5);
+
+//   z();                    // 9
+// }, 10);
+
+// setTimeout(() => {      // 2
+//   s();                  // 11
+// }, 20);
+
+// setTimeout(() => {      // 3
+//   f();                  // 4
+// });
+
+// g();                    // 5
+
+
+// f - 4, g - 5, d - 7, z - 9, n - 12, s - 11, q-12, 
+
+// f, g, d, z, n, s, q
+
+// function afterNSeconds(callback, time) {
+//   setTimeout(callback, time * 1000);
+// }
+
+let counterId;
+
+function startCounting() {
+  console.log('Starting');
+  let n = 0;
+  
+  counterId = setInterval(() => {
+      console.log('Hello!');
+      n += 2;
+      if (n >= 10) {
+        stopCounting(counterId);
+        console.log('Goodbye!');
+      }
+    }, 2000);
+}
+
+function stopCounting(id) {
+  clearInterval(id);
+}
+
+startCounting();
